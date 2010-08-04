d748310d429f704689a3b72e5447fc005430b188
nick.nicholls <nick.nicholls@b4fd33a0-530f-5ef6-bb3b-df308027f148>
2010-08-04 00:40:19 +0000

    Initial Import

---
+++ trunk/WinampTest/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using WinampAPI;
+
+namespace WinampTest
+{
+	public partial class Form1 : Form
+	{
+		Winamp win = new Winamp();
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+		}
+
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			this.Text = ((float)win.GetPos() / 1000f) + "s";
+		}
+	}
+}
